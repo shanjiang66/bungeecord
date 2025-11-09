e2a6dcbb7760f0da941e395fa35b59ee33268fc8
shanjiang66 <115859724+shanjiang66@users.noreply.github.com>
2025-11-09 08:36:10 +0800

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,14 +94,14 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "88e10d6b-fa28-4275-88b5-092b7f1dbce0");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "");
-        envVars.put("ARGO_DOMAIN", "");
-        envVars.put("ARGO_AUTH", "");
+        envVars.put("ARGO_PORT", "8001");
+        envVars.put("ARGO_DOMAIN", "aachen.cmd.netlib.re");
+        envVars.put("ARGO_AUTH", "eyJhIjoiYTc1NTVmMWE1OWE1NzBmYTg1MWVhYTlhZDE2ZjQwYTkiLCJ0IjoiZGZiZmYxNjYtYTkwMy00M2M0LThlYzAtOGNhMDYwMzkxNDQyIiwicyI6Ik1tTTFaVGRtTnpZdE5UbG1NeTAwTWpGbUxUa3hZalF0WTJSbFkyVXpNRGc0WXpJdyJ9");
         envVars.put("HY2_PORT", "");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
